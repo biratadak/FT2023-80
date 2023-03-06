29079cd152f013df7bc491cb94fcfba4c51a4f85
VEER <birat.adak@gmail.com>
2023-03-06 11:10:10 +0530

    FT2023-80: Fixed indentation and unusefull lines

--- dbconnection.php
+++ dbconnection.php
@@ -1,6 +1,6 @@
 <?php
 // Loaded all required libraries.
-require("../vendor/autoload.php"); 
+require("../vendor/autoload.php");
 //Loading .env credentials.
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
@@ -14,26 +14,26 @@ $dotenv->safeLoad();
  *  
  */
 class Database
-{   
-   
-    /**
-     * Connects with database using mysqli.
-     * 
-     * @param string $database
-     *  Store the name of database to connect.
-     * 
-     * @return mixed
-     *  returns mysqli object if connection successfull otherwise print error.
-     */
-    public function connect($database)
-    {
-        $connect = new mysqli("localhost", $_ENV['sqlUser'], $_ENV['sqlPass'], $database);
-        if ($connect->connect_error)
-            echo '<script>"Connection error:" . $connect->connect_error</script>';
-        else
-            echo '<script>console.log("Successfully connected with '.$database.' database.")</script>';
-        return $connect;
-    }
-    
+{
+
+  /**
+   * Connects with database using mysqli.
+   * 
+   * @param string $database
+   *  Store the name of database to connect.
+   * 
+   * @return mixed
+   *  returns mysqli object if connection successfull otherwise print error.
+   */
+  public function connect($database)
+  {
+    $connect = new mysqli("localhost", $_ENV['sqlUser'], $_ENV['sqlPass'], $database);
+    if ($connect->connect_error)
+      echo '<script>"Connection error:" . $connect->connect_error</script>';
+    else
+      echo '<script>console.log("Successfully connected with ' . $database . ' database.")</script>';
+    return $connect;
+  }
+
 }
 ?>
--- index.php
+++ index.php
@@ -15,26 +15,21 @@ $res = $emp->query("select employee_id from employee_salary_table order by emplo
 $val = $res->fetch_assoc()['employee_id'];
 
 // Data insertion for emp_code_table
-if( isset($_POST['emp_code']) && isset($_POST['emp_cname']) && isset($_POST['emp_dom']) && $_POST['emp_code']!="" && $_POST['emp_cname']!="" && $_POST['emp_dom']!="" ){
-    echo "inside code table if ";
-    $sql='INSERT INTO employee_code_table (employee_code,employee_code_name,employee_domain) values ("'.$_POST["emp_code"].'","'.$_POST["emp_cname"].'","'.$_POST['emp_dom'].'")';
-    $emp->query($sql);
+if (isset($_POST['emp_code']) && isset($_POST['emp_cname']) && isset($_POST['emp_dom']) && $_POST['emp_code'] != "" && $_POST['emp_cname'] != "" && $_POST['emp_dom'] != "") {
+  $sql = 'INSERT INTO employee_code_table (employee_code,employee_code_name,employee_domain) values ("' . $_POST["emp_code"] . '","' . $_POST["emp_cname"] . '","' . $_POST['emp_dom'] . '")';
+  $emp->query($sql);
 }
 
 // Data insertion for emp_salary_table
-if( isset($_POST['emp_salary']) && isset($_POST['emp_code']) && $_POST['emp_salary']!="" && $_POST['emp_code']!="" ){
-    echo "inside salary table if";
-
-    $sql='INSERT INTO employee_salary_table (employee_id,employee_salary,employee_code) values ("'.(substr($val, 0, 2) . ((int) substr($val, 2) + 1)).'","'.$_POST["emp_salary"].'","'.$_POST['emp_code'].'")';
-    $emp->query($sql);
+if (isset($_POST['emp_salary']) && isset($_POST['emp_code']) && $_POST['emp_salary'] != "" && $_POST['emp_code'] != "") {
+  $sql = 'INSERT INTO employee_salary_table (employee_id,employee_salary,employee_code) values ("' . (substr($val, 0, 2) . ((int) substr($val, 2) + 1)) . '","' . $_POST["emp_salary"] . '","' . $_POST['emp_code'] . '")';
+  $emp->query($sql);
 }
 
 // Data insertion for emp_details_table
-if(isset($_POST['emp_fname']) && isset($_POST['emp_lname']) && isset($_POST['emp_gp']) && $_POST['emp_fname']!="" && $_POST['emp_lname']!="" && $_POST['emp_gp']!=""){
-    echo "inside details table if";
-
-    $sql='INSERT INTO employee_details_table (employee_id,employee_first_name,employee_last_name,Graduation_percentile) values ("'.substr($val, 0, 2) .((int) substr($val, 2) + 1).'","'.$_POST["emp_fname"].'","'.$_POST['emp_lname'].'","'.$_POST['emp_gp'].'")';
-    $emp->query($sql);
+if (isset($_POST['emp_fname']) && isset($_POST['emp_lname']) && isset($_POST['emp_gp']) && $_POST['emp_fname'] != "" && $_POST['emp_lname'] != "" && $_POST['emp_gp'] != "") {
+  $sql = 'INSERT INTO employee_details_table (employee_id,employee_first_name,employee_last_name,Graduation_percentile) values ("' . substr($val, 0, 2) . ((int) substr($val, 2) + 1) . '","' . $_POST["emp_fname"] . '","' . $_POST['emp_lname'] . '","' . $_POST['emp_gp'] . '")';
+  $emp->query($sql);
 }
 
 
@@ -44,113 +39,116 @@ if(isset($_POST['emp_fname']) && isset($_POST['emp_lname']) && isset($_POST['emp
 <html>
 
 <head>
-    <meta charset="UTF-8" />
-    <title>SQL TASK</title>
-    <link rel="stylesheet" href="style.css">
+  <meta charset="UTF-8" />
+  <title>SQL TASK</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <form action='<?php $_SERVER["PHP_SELF"] ?>' method="POST">
-        <h3>Employee Details</h3>
+  <form action='<?php $_SERVER["PHP_SELF"] ?>' method="POST">
+    <h3>Employee Details</h3>
 
-        <label for="emp_id">ID:</label>
-        <input type="text"  value='<?php echo substr($val, 0, 2) . ((int) substr($val, 2) + 1) ?>' name="emp_id" readonly>
+    <label for="emp_id">ID:</label>
+    <input type="text" value='<?php echo substr($val, 0, 2) . ((int) substr($val, 2) + 1) ?>' name="emp_id" readonly>
 
-        <label for="emp_fname">First Name:</label>
-        <input type="text" placeholder="Tony" name="emp_fname">
+    <label for="emp_fname">First Name:</label>
+    <input type="text" placeholder="Tony" name="emp_fname">
 
-        <label for="emp_lname">Last Name:</label>
-        <input type="text" placeholder="Stark" name="emp_lname">
+    <label for="emp_lname">Last Name:</label>
+    <input type="text" placeholder="Stark" name="emp_lname">
 
-        <label for="emp_code_name">Code Name:</label>
-        <input type="text" placeholder="ru_tony" name="emp_cname">
+    <label for="emp_code_name">Code Name:</label>
+    <input type="text" placeholder="ru_tony" name="emp_cname">
 
-        <label for="emp_code">Code:</label>
-        <input type="text" placeholder="su_tony" name="emp_code">
+    <label for="emp_code">Code:</label>
+    <input type="text" placeholder="su_tony" name="emp_code">
 
-        <label for="emp_domain">Domain:</label>
-        <input type="text" placeholder="PHP" name="emp_dom">
+    <label for="emp_domain">Domain:</label>
+    <input type="text" placeholder="PHP" name="emp_dom">
 
-        <label for="emp_salary">Salary:</label>
-        <input type="text" placeholder="100k" name="emp_salary">
+    <label for="emp_salary">Salary:</label>
+    <input type="text" placeholder="100k" name="emp_salary">
 
-        <label for="emp_percentile">Graduation Percentile:</label>
-        <input type="text" placeholder="100k" name="emp_gp">
-        <div>
-            <input type="submit" value="SUBMIT" name="sub_btn">
-            <input type="submit" value="Show Table" name="show_btn">
-        </div>
+    <label for="emp_percentile">Graduation Percentile:</label>
+    <input type="text" placeholder="100k" name="emp_gp">
+    <div>
+      <input type="submit" value="SUBMIT" name="sub_btn">
+      <input type="submit" value="Show Table" name="show_btn">
+    </div>
 
 
-    </form>
-    <!-- tables section starts-->
-    <div class="tables">
+  </form>
+  <!-- tables section starts-->
+  <div class="tables">
     <?php if (isset($_POST['show_btn'])) {
-        echo '<h4>Employee Code Table</h5>';}?>
-        <div class="Scrollable">
-            
-            <div class="grid code">
-                
-                
-                <?php if (isset($_POST['show_btn'])) {
-                $table = $emp->query("select * from employee_code_table");
-                echo '<div class="grid_heads">CODE</div>
+      echo '<h4>Employee Code Table</h5>';
+    } ?>
+    <div class="Scrollable">
+
+      <div class="grid code">
+
+
+        <?php if (isset($_POST['show_btn'])) {
+          $table = $emp->query("select * from employee_code_table");
+          echo '<div class="grid_heads">CODE</div>
             <div class="grid_heads">CODE NAME</div>
             <div class="grid_Heads">DOMAIN</div>
             
             ';
-                while ($res = $table->fetch_assoc()) {
-                    foreach ($res as $r)
-                        echo '<div class="grid_elements">' . $r . '</div>';
-                }
-            }
-            ?>
-        </div>
-        </div>
-        <?php if (isset($_POST['show_btn'])) {
-        echo '<h4>Employee Salary Table</h5>';}?>
-        <div class="Scrollable">
-        <div class="grid salary">
+          while ($res = $table->fetch_assoc()) {
+            foreach ($res as $r)
+              echo '<div class="grid_elements">' . $r . '</div>';
+          }
+        }
+        ?>
+      </div>
+    </div>
+    <?php if (isset($_POST['show_btn'])) {
+      echo '<h4>Employee Salary Table</h5>';
+    } ?>
+    <div class="Scrollable">
+      <div class="grid salary">
 
-            <?php if (isset($_POST['show_btn'])) {
-                $table = $emp->query("select * from employee_salary_table");
-                echo '<div class="grid_heads">ID</div>
+        <?php if (isset($_POST['show_btn'])) {
+          $table = $emp->query("select * from employee_salary_table");
+          echo '<div class="grid_heads">ID</div>
             <div class="grid_heads">SALARY</div>
             <div class="grid_Heads">CODE</div>
             
             ';
-                while ($res = $table->fetch_assoc()) {
-                    foreach ($res as $r)
-                        echo '<div class="grid_elements">' . $r . '</div>';
-                }
-            }
-            ?>
-        </div>
-        </div>
-        <?php if (isset($_POST['show_btn'])) {
-        echo '<h4>Employee Detail Table</h5>';}?>
-        <div class="Scrollable">
-        <div class="grid detail">
+          while ($res = $table->fetch_assoc()) {
+            foreach ($res as $r)
+              echo '<div class="grid_elements">' . $r . '</div>';
+          }
+        }
+        ?>
+      </div>
+    </div>
+    <?php if (isset($_POST['show_btn'])) {
+      echo '<h4>Employee Detail Table</h5>';
+    } ?>
+    <div class="Scrollable">
+      <div class="grid detail">
 
-                <?php if (isset($_POST['show_btn'])) {
-                    $table = $emp->query("select * from employee_details_table");
-                    echo '<div class="grid_heads">ID</div>
+        <?php if (isset($_POST['show_btn'])) {
+          $table = $emp->query("select * from employee_details_table");
+          echo '<div class="grid_heads">ID</div>
             <div class="grid_heads">FIRST NAME</div>
             <div class="grid_Heads">LAST NAME</div>
             <div class="grid_Heads">GRAD.(%)</div>
             
             ';
-                    while ($res = $table->fetch_assoc()) {
-                        foreach ($res as $r)
-                            echo '<div class="grid_elements">' . $r . '</div>';
-                    }
-                }
-                ?>
-            </div>
-        </div>
-
+          while ($res = $table->fetch_assoc()) {
+            foreach ($res as $r)
+              echo '<div class="grid_elements">' . $r . '</div>';
+          }
+        }
+        ?>
+      </div>
     </div>
-    <!-- tables section ends-->
+
+  </div>
+  <!-- tables section ends-->
 
 </body>
 
